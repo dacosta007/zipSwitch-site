2a1b39145598913f8127208a9eadadba2cf21a4d
Abiodun Olakunle Dacosta <dacosta00736@gmail.com>
2020-07-21 02:21:22 +0100

    concluded the first section of the home page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,37 @@
-console.log('main.js file connected!');
+// get element by css selector
+const query = (ele, cssQry) => {
+  return ele.querySelector(cssQry);
+};
+
+// get element by its id attr
+const id = (ele) => {
+  return document.getElementById(ele);
+};
+
+const smallNav = () => {
+  let menuIconBtn = query(document, "[data-menu-btn]");
+  let navbarMenu = query(document, "[data-navbar-menu]");
+  let navbar = query(document, '.navbar');
+
+  menuIconBtn.addEventListener('click', () => {
+    // toggle navbar menu btwn display(flex & none)
+    navbar.classList.toggle('navbarBg');
+    navbarMenu.classList.toggle('showNav');
+  });
+}
+
+
+// handles navbar transparency
+window.addEventListener('scroll', () => {
+  let windowScrl = window.scrollY;
+  let navbar = query(document, '.navbar');
+  if(windowScrl >= 612) {
+    navbar.classList.add("active");
+  } else {
+    navbar.classList.remove('active');
+  }
+});
+
+
+// Init small device navbar
+document.addEventListener('load', smallNav());
