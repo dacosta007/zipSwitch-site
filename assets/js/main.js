11a9d858074962f0596514bb24df591b96d85c46
Abiodun Olakunle Dacosta <dacosta00736@gmail.com>
2020-07-23 12:40:29 +0100

    Image slide for homepage first section

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,10 @@ const query = (ele, cssQry) => {
   return ele.querySelector(cssQry);
 };
 
+const queryAll = (ele, cssQry) => {
+  return ele.querySelectorAll(cssQry);
+};
+
 // get element by its id attr
 const id = (ele) => {
   return document.getElementById(ele);
@@ -19,6 +23,24 @@ const smallNav = () => {
     navbarMenu.classList.toggle('showNav');
   });
 }
+
+// handles img slide on the page first section
+const imgSlideIndx = 0;
+const showImgSlides = () => {
+  let imgIndicators = queryAll(document, '.img-slide-indicator');
+  let imgContainers = queryAll(document, '.device-img');
+
+  // increment imgSlideIndx value
+  imgSlideIndx++;
+  // check to see if reach end of img count: to repeat action
+  if(imgSlideIndx > imgContainers.length) { imgSlideIndx = 0 }
+
+  imgContainers.forEach(img => {
+    
+    console.log(img.children[0].getAttribute('alt'));
+  });
+  // console.log(imgs.length, imgIndicators.length, imgSlideIndx);
+};
 
 
 // handles navbar transparency
