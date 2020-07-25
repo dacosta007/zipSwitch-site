8541cb2c7c891c6269398446508ec403a78c554e
Abiodun Olakunle Dacosta <dacosta00736@gmail.com>
2020-07-25 16:09:59 +0100

    fine-tune section contanier CSS class for responsiveness

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,14 +18,21 @@ const smallNav = () => {
   let navbar = query(document, '.navbar');
 
   menuIconBtn.addEventListener('click', () => {
-    // toggle navbar menu btwn display(flex & none)
+    if(menuIconBtn.classList.contains('ti-menu')) {
+      menuIconBtn.classList.remove('ti-menu');
+      menuIconBtn.classList.add('ti-close');
+    } else {
+      menuIconBtn.classList.add('ti-menu');
+      menuIconBtn.classList.remove('ti-close');
+    }
+    // menuIconBtn.classList.toggle('-');
     navbar.classList.toggle('navbarBg');
     navbarMenu.classList.toggle('showNav');
   });
 }
 
 // handles img slide on the page first section
-const imgSlideIndx = 0;
+let imgSlideIndx = 0;
 const showImgSlides = () => {
   let imgIndicators = queryAll(document, '.img-slide-indicator');
   let imgContainers = queryAll(document, '.device-img');
@@ -33,16 +40,23 @@ const showImgSlides = () => {
   // increment imgSlideIndx value
   imgSlideIndx++;
   // check to see if reach end of img count: to repeat action
-  if(imgSlideIndx > imgContainers.length) { imgSlideIndx = 0 }
+  if(imgSlideIndx > (imgContainers.length - 1)) { imgSlideIndx = 0; }
 
-  imgContainers.forEach(img => {
-    
-    console.log(img.children[0].getAttribute('alt'));
-  });
-  // console.log(imgs.length, imgIndicators.length, imgSlideIndx);
+  // hide all the img at initialization of the page load
+  for(let i = 0; i < imgContainers.length; i++) {
+    imgContainers[i].classList.remove('active');
+    imgIndicators[i].classList.remove('active');
+  }
+  // display img  required img (base on img slide count)
+  imgContainers[imgSlideIndx].classList.add('active');
+  imgIndicators[imgSlideIndx].classList.add('active');
+
+  // auto slide image, and show indicator after 6seconds
+  setTimeout(showImgSlides, 6000);
 };
 
 
+/* ***** Event Handlers ***** */
 // handles navbar transparency
 window.addEventListener('scroll', () => {
   let windowScrl = window.scrollY;
@@ -54,6 +68,7 @@ window.addEventListener('scroll', () => {
   }
 });
 
+// document.addEventListener('DOMContentLoaded', showImgSlides);
 
 // Init small device navbar
 document.addEventListener('load', smallNav());
