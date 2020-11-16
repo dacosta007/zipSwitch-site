be0b0c2b60add55ce89493f06aee84949a343201
Abiodun Olakunle Dacosta <dacosta00736@gmail.com>
2020-11-16 18:27:28 +0100

    conclude homepage structural design

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,6 +55,91 @@ const showImgSlides = () => {
   setTimeout(showImgSlides, 6000);
 };
 
+/* ***** Method for image slides ***** */
+// image slide start counter
+let imgSlideCount = 0;
+const imgSlides = () => {
+  let images = queryAll(document, '.device-img-slide-preview-container img');
+  let indicators = queryAll(document, '.img-preview-indicator-container .img-preview-indicator');
+
+  // increment img slide count
+  imgSlideCount++;
+  // check to see if reach end of img count: to repeat action
+  if(imgSlideCount > (images.length - 1)) { imgSlideCount = 0; }
+
+  // remove the 'active' class from every element
+  for(let i = 0; i < images.length; i++) {
+    images[i].classList.remove('active');
+    indicators[i].classList.remove('active');
+  }
+
+  // display required img (base on img slide count)
+  images[imgSlideCount].classList.add('active');
+  indicators[imgSlideCount].classList.add('active');
+  
+  // auto slide image, and show indicator after specified seconds
+  setTimeout(imgSlides, 4000);
+};
+/* ***** Method for image slides ends ***** */
+
+/* **** For previewing device picture in a modal window **** */
+const closeModal = () => {
+  const modalWindow = query(document, '.modal');
+  modalWindow.style.display = 'none';
+};
+
+const showImgModal = () => {
+  // device images
+  const images = imgsToPreview;
+  // modal window, modal big img content, & modal list of images
+  let modalWindow = query(document, '.modal');
+  let modalImg = query(document, '.image-preview img');
+  let modalImgList = queryAll(document, '[data-modal-img-list]');
+
+  images.forEach(img => {
+    img.addEventListener('click', () => {
+      // set modal big img content src
+      modalImg.src = img.src;
+      // remove all active class from list of imgs & put it on displayed img among the list
+      for (let i = 0; i < modalImgList.length; i++) {
+        modalImgList[i].classList.remove('active');
+      }
+      modalImgList[img.dataset.deviceImg].classList.add('active');
+    
+      // display the modal window
+      modalWindow.style.display = 'flex';
+    });
+  });
+};
+
+// Method to handle modal image slide
+const modalImgListSlide = () => {
+  // modal large img
+  let modalImg = query(document, '.image-preview img');
+  // modal list of imgs
+  let modalImgList = queryAll(document, '[data-modal-img-list]');
+
+  // add click event to all device img list: to display desired img
+  modalImgList.forEach((img) => {
+    img.addEventListener('click', () => {
+      // remove 'active' class from all the imgs
+      for (let i = 0; i < modalImgList.length; i++) {
+        modalImgList[i].classList.remove('active');
+      }
+      // add 'active' class to selected img
+      img.classList.add('active');
+      // show the img selected in the big modal content container
+      modalImg.src = img.src;
+    });
+  });
+};
+
+let imgsToPreview = queryAll(document, '.device-img-slide-preview-container img');
+if (imgsToPreview && imgsToPreview != undefined) {
+  showImgModal();
+  modalImgListSlide();
+}
+/* **** For previewing device picture in a modal window ends **** */
 
 /* ***** Event Handlers ***** */
 // handles navbar transparency
@@ -68,7 +153,19 @@ window.addEventListener('scroll', () => {
   }
 });
 
-// document.addEventListener('DOMContentLoaded', showImgSlides);
-
 // Init small device navbar
 document.addEventListener('load', smallNav());
+
+// Init images slide animation effects
+document.addEventListener('DOMContentLoaded', () => {
+  // callout image showcase slide 
+  showImgSlides();
+
+  // for image slide preview
+  imgSlides();
+});
+
+// close modal window
+const closeModalBtn = query(document, '.close-modal');
+closeModalBtn.addEventListener('click', closeModal);
+/* ***** Event Handlers Ends ***** */
